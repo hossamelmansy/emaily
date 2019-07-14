0f7444666ea070d22a240e3a072669bc611c30c0
Hossam ELMansy <hossamelmansy.developer@gmail.com>
2019-07-14 11:38:38 +0200

    adding auth with Google

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,31 @@
 require('dotenv').config();
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send({ hi: 'there' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+      done();
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 app.listen(process.env.PORT);
