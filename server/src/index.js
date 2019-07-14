6bf2f43b170de0104318393d9f5b8097a19b3783
Hossam ELMansy <hossamelmansy.developer@gmail.com>
2019-07-14 22:22:35 +0200

    configure express with client in production

--- server/src/index.js
+++ server/src/index.js
@@ -23,4 +23,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV == 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will server up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(process.env.PORT);
