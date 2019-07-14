e08e4a01bd0f1dc1404603da721e3c1e2a460005
Hossam ELMansy <hossamelmansy.developer@gmail.com>
2019-07-14 11:59:32 +0200

    adding connect to mongodb

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 require('./services/passport');
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 
 const app = express();
 require('./routes/authRoutes')(app);
