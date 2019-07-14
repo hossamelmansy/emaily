41ec416ca999ecccd9f3627401ba9e05fea22070
Hossam ELMansy <hossamelmansy.developer@gmail.com>
2019-07-14 15:37:32 +0200

    adding routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import RootReducer from '../reducers';
 
-const store = createStore(RootReducer, applyMiddleware());
+function Header() {
+  return <h2>Header</h2>;
+}
+
+function Dashboard() {
+  return <h2>Dashboard</h2>;
+}
+
+function SurveyNew() {
+  return <h2>SurveyNew</h2>;
+}
+
+function Landing() {
+  return <h2>Landing</h2>;
+}
 
 function App() {
   return (
-    <Provider store={store}>
-      <div>Hello World</div>
+    <Provider store={createStore(RootReducer, applyMiddleware())}>
+      <BrowserRouter>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/surveys" component={Dashboard} />
+        <Route path="/surveys/new" component={SurveyNew} />
+      </BrowserRouter>
     </Provider>
   );
 }
